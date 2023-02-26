1ab5200460ebcf811541e578c57ef5a4ee0e8734
diphokoo <diphokoo@outlook.com>
2023-02-27 01:36:05 +0200

    markup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import logo from './react-logo.png';
 import './App.css';
 
 function App() {
   return (
-    <div>
-        <p>test</p>
+    <div className="App">
+      <img src={logo} width="40px" alt="logo" />
+      <h1>Fun facts about React</h1>
+      <ul>
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 100K stars on Github</li>
+        <li>Is maintained by Facebook</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
+      </ul>
     </div>
   );
 }
