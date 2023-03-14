4d75af55b12ad2081e2b293569a6e406fea8319e
diphokoo <diphokoo@outlook.com>
2023-03-15 01:20:27 +0200

    html content

--- src/component/Main.jsx
+++ src/component/Main.jsx
@@ -1,5 +1,14 @@
 export default function Main() {
     return (
-        <h3>Main content</h3>
+        <main>
+            <h1>Fun facts about React</h1>
+            <ul>
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has welll over 100K stars on GitHub</li>
+                <li>Is maintained by Facebook</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+        </main>
     )
 }
